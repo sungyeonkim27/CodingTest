c645b3991bcf8010b45a366432d6ae2d71ff3d62
sungyeon kim <awers1010@gmail.com>
2025-04-07 21:15:47 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/프로그래머스코드챌린지예선/달리기경주.java
@@ -0,0 +1,26 @@
+package 프로그래머스코드챌린지예선;
+
+import java.util.HashMap;
+
+public class 달리기경주 {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> rankMap = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            rankMap.put(players[i], i);
+        }
+        for (String call : callings) {
+            int curIndex = rankMap.get(call);
+            String prevPlayer = players[curIndex - 1];
+
+            players[curIndex - 1] = call;
+            players[curIndex] = prevPlayer;
+
+            rankMap.put(call, curIndex - 1);
+            rankMap.put(prevPlayer, curIndex);
+        }
+
+
+        return players;
+    }
+}
